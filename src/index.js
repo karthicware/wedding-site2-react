d75275defee5f3c43ed14c623f998ad48565ca5d
natarajan <karthicware@gmail.com>
2019-06-06 20:04:49 +0530

    src folder moved to out

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+import Header from "./pages/header";
+import HappyCouple from "./pages/happy-couple";
+import TwitterCards from "./pages/twitter-cards";
+import Quote from "./pages/quote";
+import Footer from "./pages/footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Header />
+      <div className="main main-raised">
+        <div
+          className="container-fluid"
+          style={{ padding: "0px 120px 0px 120px" }}
+        >
+          <HappyCouple />
+          <TwitterCards />
+          <Quote />
+        </div>
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
